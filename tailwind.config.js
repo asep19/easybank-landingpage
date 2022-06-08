1b9e0570395732b97bd9a1e14013aa036c01e5de
asep19 <aseprendi487@gmail.com>
2022-06-08 22:29:57 +0700

    setup new color theme in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,17 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
+    colors: {
+      white: "hsl(0, 0%, 100%)"
+    },
     extend: {
       colors: {
-        
+        darkblue: "hsl(233, 26%, 24%)",
+        limegreen: "hsl(136, 65%, 51%)",
+        brightcyan: "hsl(192, 70%, 51%)",
+        grayishblue: "hsl(233, 8%, 62%)",
+        lightgrayishblue: "hsl(220, 16%, 96%)",
+        verylightgray: "hsl(0, 0%, 98%)",
       }
     },
   },
